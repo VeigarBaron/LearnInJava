0c1ac26b32e54595439522bd447cc5c30395c92f
Veigar <30363166+VeigarBaron@users.noreply.github.com>
2020-10-19 18:02:22 +0800

    修改

--- src/javabase/TestFatherJava.java
+++ src/javabase/TestFatherJava.java
@@ -1,5 +1,7 @@
 package javabase;
 
+import java.util.*;
+
 /**
  * @author VeigarBaron
  * @date 2020/8/19 13:46
@@ -9,7 +11,19 @@ public class TestFatherJava {
     private int intJava = 1;
     public String strJava = "str";
 
-    private void method() {
-        System.out.println("asdf");
+    public void testMethod(){
+//        Iterator
+        ArrayList listA = new ArrayList();
+        LinkedList listL = new LinkedList();
+        Vector vector = new Vector();
+
+        HashMap map = new HashMap();
+        Hashtable hashtable = new Hashtable();
+        TreeMap treeMap = new TreeMap();
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+
+        HashSet hashSet = new HashSet();
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        TreeSet treeSet = new TreeSet();
     }
 }
