e6a92fa8fd7389233ddce988781b217da3715436
Veigar <30363166+VeigarBaron@users.noreply.github.com>
2021-02-01 18:03:14 +0800

    力扣刷题

---
+++ src/algorithm算法/力扣100题/MaxDepth.java
@@ -0,0 +1,31 @@
+package algorithm算法.力扣100题;
+
+/**
+ * @author VeigarBaron
+ * @date 2021/2/1 17:36
+ * @Description *
+ */
+public class MaxDepth {
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        // 递归，深度
+        int depthL = 0;
+        int depthR = 0;
+
+        return 0;
+    }
+}
