505c92bac2159c135b40730fd0287bcf87ec42f2
Veigar <30363166+VeigarBaron@users.noreply.github.com>
2021-02-02 16:25:05 +0800

    力扣刷题

---
+++ src/algorithm算法/力扣100题/IsSymmetric.java
@@ -0,0 +1,34 @@
+package algorithm算法.力扣100题;
+
+/**
+ * @author VeigarBaron
+ * @date 2021/2/2 15:02
+ * @Description 对称二叉树
+ */
+public class IsSymmetric {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+
+        return false;
+    }
+
+    public TreeNode symmetric(TreeNode root) {
+
+        return root;
+    }
+
+}
